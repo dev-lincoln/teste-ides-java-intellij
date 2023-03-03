9135d5fdd4c99aeb01a92c7fa9eea8a1303920b1
dev-lincoln <lincoln.dev@outlook.com>
2023-03-03 17:10:48 -0300

    criando class Livros

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,10 +7,47 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livros livro = new Livros("O pequeno principe", 300);
+        System.out.println(livro);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b) );*/
-
-
     }
+
 }
+
+    class Livros {
+        private String nome;
+        private Integer numPages;
+
+        public Livros(String nome, Integer numPages) {
+            this.nome = nome;
+            this.numPages = numPages;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPages() {
+            return numPages;
+        }
+
+        public void setNumPages(Integer numPages) {
+            this.numPages = numPages;
+        }
+
+        @Override
+        public String toString() {
+            return "Livros{" +
+                    "nome='" + nome + '\'' +
+                    ", numPages=" + numPages +
+                    '}';
+        }
+    }
+
